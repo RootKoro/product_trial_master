e99a91ff2127e36f42075a44160ae562b636a291
Cyr Mathieu GUEYE <mgueye@eurocybergroup.fr>
2024-09-09 20:15:27 +0200

    update: configured the product routes for swagger documentation

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import errorHandler from './middleware/errorhandler.js';
 import productsRouter from './routes/products.route.js';
 import { sequelize } from './config/db.conf.js';
 import { Product } from './models/products.model.js';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { format } from 'sequelize/lib/utils';
 
 dotenv.config();
 sequelize.authenticate()
@@ -24,13 +27,75 @@ db.sequelize.sync({ force: false })
 
 let app = express();
 const port  = process.env.PORT || 3000;
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Product Trial Master",
+            version: "0.1",
+        },
+        components: {
+            schemas: {
+                Product: {
+                    type: 'object',
+                    required: [
+                        'code',
+                        'name',
+                        'image',
+                        'category',
+                        'price',
+                        'quantity',
+                        'internalReference',
+                        'shellId',
+                    ],
+                    properties: {
+                        id: { type: 'number' },
+                        code: { type: 'string' },
+                        name: { type: 'string' },
+                        description: { type: 'string' },
+                        image: { type: 'string' },
+                        category: { type: 'string' },
+                        price: { type: 'number' },
+                        quantity: { type: 'number' },
+                        internalReference: { type: 'string' },
+                        shellId: { type: 'number' },
+                        inventoryStatus: { 
+                            type: 'string',
+                            enum: ['INSTOCK', 'LOWSTOCK', 'OUTOFSTOCK'],
+                        },
+                        rating: { type: 'number' },
+                        createdAt: { 
+                            type: 'string',
+                            format: 'datetime',
+                        },
+                        updatedAt: { 
+                            type: 'string',
+                            format: 'datetime',
+                        },
+                    }
+                }
+            }
+        }
+    },
+    apis: ["./routes/*.route.js"],
+}
+
+const swaggerSpec = swaggerJsdoc(options)
 
 app.use(express.json());
 app.use("/products", productsRouter)
 app.use(errorHandler);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', function(req, res) {
     res.send('Product Trial Master, solved by Cyr Mathieu GUEYE!');
 });
+app.get('docs.json', (res, req) => {
+    res.header('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+})
 
-app.listen(port, () => console.log('Server listening on port ' + port));
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+    console.log(`Swagger Documentation available at http://localhost:${process.env.PORT}/docs`);
+});
--- routes/products.route.js
+++ routes/products.route.js
@@ -9,14 +9,185 @@ import {
 
 const productsRouter = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - code
+ *         - name
+ *         - image
+ *         - category
+ *         - price
+ *         - quantity
+ *         - internalReference
+ *         - shellId
+ *         - inventoryStatus
+ *       properties:
+ *         id:
+ *           type: number
+ *         code:
+ *           type: string
+ *         name:
+ *           type: string
+ *         description:
+ *           type: string
+ *         image:
+ *           type: string
+ *         category:
+ *           type: string
+ *         price:
+ *           type: number
+ *         quantity:
+ *           type: number
+ *         internalReference:
+ *           type: string
+ *         shellId:
+ *           type: number
+ *        inventoryStatus:
+ *           type: string
+ *           enum:
+ *              - INSTOCK
+ *              - LOWSTOCK
+ *              - OUTOFSTOCK
+ *         rating:
+ *           type: number
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: The products managing API
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Returns the list of all the products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: The list of the products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
 productsRouter.route('/').get(getProducts);
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get the product by id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *     responses:
+ *       200:
+ *         description: The product description by id
+ *         contents:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The product was not found
+ */
 productsRouter.route('/:id').get(getProduct);
 
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: The product was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Some server error
+ */
 productsRouter.route('/').post(createProduct);
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   patch:
+ *     summary: Update the product by the id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: The product was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The product was not found
+ *       500:
+ *         description: Some error happened
+ */
 productsRouter.route('/:id').patch(updateProduct);
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Remove the product by id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *     responses:
+ *       204:
+ *         description: no response returned
+ *       404:
+ *         description: The product was not found
+ */
 productsRouter.route('/:id').delete(deleteProduct);
 
 export default productsRouter;
