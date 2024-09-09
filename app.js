d5539b5306bf7c6728347191f65ace5c822ec023
Cyr Mathieu GUEYE <mgueye@eurocybergroup.fr>
2024-09-09 18:41:03 +0200

    update: setted up all the needed configurations for the app the run correctly

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
 import express from 'express';
-import { errorHandler } from './middleware/error.handler';
-import productsRouter from './routes/products.route';
+import { DataTypes, Sequelize } from 'sequelize';
+import errorHandler from './middleware/errorhandler.js';
+import productsRouter from './routes/products.route.js';
+import { sequelize } from './config/db.conf.js';
+import { Product } from './models/products.model.js';
+
+dotenv.config();
+sequelize.authenticate()
+    .then(() => console.log("Connection to the database established."))
+    .catch(e => console.error("Error connecting to the database"))
+
+let db = {}
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.products = new Product(sequelize, DataTypes)
+
+db.sequelize.sync({ force: false })
+    .then(() => console.log("Database syncrhonized successfully!"))
+    .catch(e => console.error(e));
 
 let app = express();
 const port  = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use("/products", productsRouter)
+app.use(errorHandler);
+
 app.get('/', function(req, res) {
     res.send('Product Trial Master, solved by Cyr Mathieu GUEYE!');
 });
-
-app.use(express.json());
-app.use("api/v1/products", productsRouter)
-app.use(errorHandler);
 
 app.listen(port, () => console.log('Server listening on port ' + port));
